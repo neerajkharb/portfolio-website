a66c489377fee6d4700f388435f3b84a581fbf73
Neeraj Kharb <154985693+neerajkharb@users.noreply.github.com>
2024-02-07 18:20:03 +0100

    Responsive

--- js/main.js
+++ js/main.js
@@ -17,4 +17,45 @@ burgerBtn.onclick = function () {
 }
 
 
+let isBurgerClose = false;
+
+burgerMenu.onclick = function () {
+    if (!isBurgerClose) {
+        burgerMenu.style.display = "block";
+        burgerBtn.style.backgroundPosition = "center left 50px, centre";
+        isBurgerClose = true;
+    }
+    else if (isBurgerClose) {
+        burgerMenu.style.display = "none";
+        burgerBtn.style.backgroundPosition = "centre. centre left 50px";
+        isBurgerClose = false;
+    }
+}
+
+
+
+
+
+
+
+
+/*
+var navLink = document.querySelector(".nav-link");
+
+navLink.addEventListener("click", menuClose);
+
+function menuClose() {
+    burgerMenu.classList.add("change");
+}
+*/
+
+
+
+
+
+
+
+ 
+
+
     
